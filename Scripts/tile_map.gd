dbcd8144d6efbdd1758d8c143df39fd1d3040317
Avvienash <avvienash@gmail.com>
2025-11-30 11:54:07 +1100

    Bom zoom out logic

--- Scripts/tile_map.gd
+++ Scripts/tile_map.gd
@@ -5,14 +5,27 @@ extends Node
 @onready var numbers: TileMapLayer = $numbers
 @onready var player: CharacterBody2D = $"../Player"
 @onready var camera_2d: Camera2D = $"../Player/Camera2D"
+@onready var mine_timer: Timer = $mine_timer
+@onready var wait_timer: Timer = $wait_timer
 
 @export var mine_scene: PackedScene
 @export var house_scene: PackedScene
 
 
+var difficulty_levels = {
+	"Easy": {"rows": 9, "cols": 9, "mines": 10, "zoom": 1.05},
+	"Medium": {"rows": 16, "cols": 16, "mines": 40, "zoom": 0.5},
+	"Hard": {"rows": 24, "cols": 24, "mines": 99, "zoom": 0.4}
+}
+
 var rows = 10
 var cols = 10
 var mines = 10
+var game_lost = false
+var game_won = false
+var mines_destroyed = 0
+var sorted_mines = []
+var zoom_out = 1.05
 
 # 2D Array to hold mine positions
 var mines_array = []
@@ -21,21 +34,59 @@ var mines_uncovered_array = []
 
 
 var tile_size = 64
-
 const FOAM_ROCK_COUNT = 15
 const FOAM_ROCK_DISTANCE = 3 # Max distance from edge to place foam rocks
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	setupMines()
-	drawMap()
-	# Position player at random location within the map bounds
-	initPlayer()
+	loadGame("Hard")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+func _process(delta: float) -> void:
+	# Zoom camera based out when gane is lost or won
+	if game_lost or game_won:
+		# pause the player movement
+		player.pause_game()
+		# move cmera to (center of map)
+		# so get target position of center of map relavetive to player position
+		var target_position = Vector2((rows * tile_size) / 2, (cols * tile_size) / 2)
+		var direction = target_position - (player.position + camera_2d.position)
+		var distance = direction.length()
+		if distance > 10:
+			direction = direction.normalized()
+			camera_2d.position += direction * distance * delta * 0.4
+		# zoom out camera
+		if camera_2d.zoom.x > zoom_out:
+			camera_2d.zoom -= Vector2(1,1) * delta * 0.2
+
+
+
+
+
+
+
+func loadGame(difficulty: String):
+
+	var level = difficulty_levels[difficulty]
+	rows = level["rows"]
+	cols = level["cols"]
+	mines = level["mines"]
+
+	game_lost = false
+	game_won = false
+	mines_destroyed = 0
+	camera_2d.position = Vector2(0,0)
+	camera_2d.zoom = Vector2(1.7,1.7)
+	zoom_out = level["zoom"]
+
+	player.resume_game()
+	
+	setupMines()
+	drawMap()
+	initPlayer()
+
 
 func initPlayer():
 	var flag = false
@@ -197,11 +248,13 @@ func cellSelected(x,y) -> void:
 	mines_uncovered_array[x][y] = true
 	# first check if it's a mine
 	if mines_array[x][y] == -1:
-		print("Game Over! Stepped on a mine at (", x, ",", y, ")")
 		
 		# Handle game over logic here
 		if mines_scene_array[x][y] != null:
 				mines_scene_array[x][y]._destroy()
+
+		gameOver()
+	
 	else:
 		print("Safe cell at (", x, ",", y, ") with value: ", mines_array[x][y])
 		# Handle safe cell logic here
@@ -212,8 +265,56 @@ func cellSelected(x,y) -> void:
 			# Reveal adjacent cells recursively
 			revealAdjacentCells(x, y)
 
+		# Check for win condition here
+		for i in rows:
+			for j in cols:
+				if mines_array[i][j] != -1 and not mines_uncovered_array[i][j]:
+					return # Still safe cells left
+		gameWon()
+
+func gameWon():
+	print("Congratulations! You've cleared all safe cells!")
+	game_won = true
+
+func gameOver():
+	print("Game Over! You hit a mine.")
+	game_lost = true
+	# Reveal all mines based on disatnce from player
+	var player_cell = Vector2i(int(player.position.x) / tile_size, int(player.position.y) / tile_size)
+	var distances = []
+	var mine_positions = []
+	for x in rows:
+		for y in cols:
+			if mines_array[x][y] == -1:
+				var dist = player_cell.distance_to(Vector2i(x,y))
+				distances.append(dist)
+				mine_positions.append(Vector2i(x,y))
+	# Sort mines by distance
+	sorted_mines = mine_positions.duplicate()
+	sorted_mines.sort_custom(func(a, b):
+		var dist_a = player_cell.distance_to(a)
+		var dist_b = player_cell.distance_to(b)
+		return dist_a - dist_b
+	)
+	
+	# Reveal the first Mine and start the timer
+	wait_timer.start()
+	
+func destroyMine():
+	
+	if mines_destroyed >= mines:
+		return
+	if mines_scene_array[sorted_mines[mines_destroyed].x][sorted_mines[mines_destroyed].y] == null:
+		mines_destroyed += 1
+		return
+	mines_scene_array[sorted_mines[mines_destroyed].x][sorted_mines[mines_destroyed].y]._destroy()
+	mine_timer.start()
+	mines_destroyed += 1
+	
+
+
 func revealAdjacentCells(x, y) -> void:
-	print("Revealing adjacent cells for (", x, ",", y, ")")
+	#print("Revealing adjacent cells for (", x, ",", y, ")")
 	var directions = [
 		Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1),
 		Vector2i(-1, 0),				Vector2i(1, 0),
@@ -230,7 +331,7 @@ func revealAdjacentCells(x, y) -> void:
 			# Only reveal if not already uncovered
 			if mines_uncovered_array[new_x][new_y] ==  false:
 				mines_uncovered_array[new_x][new_y] = true
-				print("Revealed cell at (", new_x, ",", new_y, ") with value: ", mines_array[new_x][new_y])
+				#print("Revealed cell at (", new_x, ",", new_y, ") with value: ", mines_array[new_x][new_y])
 
 				
 				if mines_scene_array[new_x][new_y] != null:
@@ -240,3 +341,14 @@ func revealAdjacentCells(x, y) -> void:
 					revealAdjacentCells(new_x, new_y)
 
 	
+
+
+func _on_mine_timer_timeout() -> void:
+	while mines_destroyed < mines:
+		destroyMine()
+		return
+		
+func _on_wait_timer_timeout() -> void:
+	if game_lost:
+		destroyMine()
+		
